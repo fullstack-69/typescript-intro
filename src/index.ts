dccc93c5fee6df6d0447692eb2cf6d2d88daf9a2
Nirand Pisutha-Arnond <nnnpooh@gmail.com>
2026-06-23 09:25:18 +0700

    Update

--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,30 @@
-//  1
-// function sayHello(name: string) {
-//   console.log("Hello " + name);
-// }
+//  Example 1
+function sayHello(name: string) {
+  console.log("Hello " + name);
+}
 
 // sayHello("World");
 
-// 2
-async function getData() {
-  const res = await fetch("https://jsonplaceholder.typicode.com/todos/1");
-  return await res.json();
-}
+// Example 2
+// async function getData() {
+//   const res = await fetch("https://jsonplaceholder.typicode.com/todos/1");
+//   return await res.json();
+// }
 
-getData().then((data) => console.log(data));
+// getData().then((data) => console.log(data));
 
-// // 3
-// import { name } from "./lib.js";
+// Example 3
+import { name } from "./lib.js";
 
-// sayHello(name);
+sayHello(name);
 
-// // 4
-// import * as fs from "fs";
-// // Fix __dirname is not defined error https://stackoverflow.com/a/64383997
-// import { fileURLToPath } from "url";
-// import { dirname } from "path";
-// const __filename = fileURLToPath(import.meta.url);
-// const __dirname = dirname(__filename);
+// Example 4
+import * as fs from "fs";
+// Fix __dirname is not defined error https://stackoverflow.com/a/64383997
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
-// const dir = fs.readdirSync(__dirname);
-// console.log(dir);
+const dir = fs.readdirSync(__dirname);
+console.log(dir);
